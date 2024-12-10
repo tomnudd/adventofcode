22d109d5047d6a7501e983ba089b3063a24bd4e5
tomnudd <thomasgnudd@gmail.com>
2024-12-10 23:11:39 +0000

    day-5: part 2, swap problem elements until it works

--- day-05/day-05.c
+++ day-05/day-05.c
@@ -9,10 +9,9 @@ struct hashy {
     int key;
     int *val;
     int count;
+    int orig;
     struct hashy *next;
 };
-
-struct hashy *table[ARRAY_SIZE] = {NULL};
 
 int hash(int i)
 {
@@ -32,7 +31,7 @@ struct hashy *lookup(struct hashy **tab, int i)
     return NULL;
 }
 
-struct hashy *add(struct hashy **tab, int key, int val)
+struct hashy *add(struct hashy **tab, int key, int val, int orig)
 {
     struct hashy *p;
     int hashValue;
@@ -51,6 +50,8 @@ struct hashy *add(struct hashy **tab, int key, int val)
         p->val[0] = val;
         p->count = 1;
 
+        p->orig = orig;
+
         tab[hashValue] = p;
     }
     else
@@ -60,17 +61,74 @@ struct hashy *add(struct hashy **tab, int key, int val)
         p->val = realloc(p->val, sz * sizeof(int));
         p->val[sz-1] = val;
         p->count = sz;
+
+        p->orig = orig;
     }
 
     return p;
 }
 
+void freeHashy(struct hashy **tab)
+{
+    int i;
+    for (i = 0; i < ARRAY_SIZE; i++)
+    {
+        struct hashy *cur = tab[i];
+        while (cur != NULL)
+        {
+            struct hashy *next = cur->next;
+
+            if (cur->val != NULL)
+            {
+                free(cur->val);
+            }
+
+            free(cur);
+
+            cur = next;
+        }
+    }
+}
+
+int checkArr(struct hashy **tab, int *z, int len, int *fi, int *fo)
+{
+    struct hashy *temp[ARRAY_SIZE] = {NULL};
+
+    int p1 = 1;
+    int i;
+    for (i = 0; i < len; i++)
+    {
+        int val = z[i];
+        struct hashy *t = lookup(temp, val);
+        if (t != NULL)
+        {
+            p1 = 0;
+            *fi = i;
+            *fo = t->orig;
+        }
+
+        // Add the values we cannot now see (given val has been seen) to our hash
+        struct hashy *u = lookup(tab, val);
+        if (u != NULL)
+        {
+            int j;
+            for (j = 0; j < u->count; j++)
+            {
+                add(temp, u->val[j], 1, i);
+            }
+        }
+    }
+
+    freeHashy(temp);
+
+    return p1;
+}
+
 // If we have seen 47|53, this means 47 must be before 53.
 // Therefore, if we see 53, we cannot see 47 later
 // 53 is the key, its values cannot be seen later
-int processLine(char *line)
+void processLine(struct hashy **tab, char *line, int *part1, int *part2)
 {
-    struct hashy *temp[ARRAY_SIZE] = {NULL};
     int z[64] = {0};
 
     // for thing we see
@@ -84,29 +142,40 @@ int processLine(char *line)
         int val;
         if (sscanf(tok, "%d", &val) == 1)
         {
-            if (lookup(temp, val))
-            {
-                return 0;
-            }
-
             z[len] = val;
             len++;
-            
-            // Add the values we cannot now see (given val has been seen) to our hash
-            struct hashy *u = lookup(table, val);
-            if (u != NULL)
-            {
-                int i;
-                for (i = 0; i < u->count; i++)
-                {
-                    add(temp, u->val[i], 1);
-                }
-            }
         }
         tok = strtok(NULL, ",");
     }
 
-    return z[len/2];
+    int fi = -1;
+    int fo = -1;
+    int p1 = 1;
+    p1 = checkArr(tab, z, len, &fi, &fo);
+
+    if (p1)
+    {
+        *part1 += z[len/2];
+        *part2 += 0;
+        return;
+    }
+
+    // track the following:
+        // fi: index of problem
+        // fo: index of origin of problem (what caused the element at z[fi] to fail)
+    // idea: swap these elements in z until it works
+    while (!p1)
+    {
+        int tmp = z[fi];
+        z[fi] = z[fo];
+        z[fo] = tmp;
+
+        fi = -1;
+        fo = -1;
+        p1 = checkArr(tab, z, len, &fi, &fo);
+    }
+
+    *part2 += z[len/2];
 }
 
 int main(int argc, char **argv)
@@ -126,7 +195,12 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    int count = 0;
+    struct hashy *table[ARRAY_SIZE] = {NULL};
+
+    int part1 = 0;
+    int part2 = 0;
+
+    // read and parse
     char line[128];
     int rules = 1;
     while (fgets(line, sizeof(line), input) != NULL)
@@ -144,20 +218,24 @@ int main(int argc, char **argv)
             if (sscanf(line, "%d|%d", &p1, &p2) == 2)
             {
                 // Add multiplication to sum
-                add(table, p2, p1);
+                add(table, p2, p1, 0);
             }
         }
         else
         {
-            count += processLine(line);
+            processLine(table, line, &part1, &part2);
 
-            // part 2 = tsort?
+            // alternative ideas: (1) build graph then tsort, (2) qsort z arrays based on rules
         }
 
     }
 
-    printf("PART 1: %d\n", count);
-
+    // close
     fclose(input);
+
+
+    printf("PART 1: %d\n", part1);
+    printf("PART 2: %d\n", part2);
+
 
 }
