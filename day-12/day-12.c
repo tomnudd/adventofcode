09f9a147a45aa3d2a8fe6ecf1b18b52c910df475
Tom Nudd <thomasgnudd@gmail.com>
2024-12-27 23:57:17 +0000

    day 12: c, part 2, determine if start of an perimeter edge

--- day-12/day-12.c
+++ day-12/day-12.c
@@ -7,7 +7,8 @@
 struct element
 {
     char type;
-    int contribution;
+    int contribution; // part 1
+    int contribution2; // part 2
     int region;
 };
 
@@ -15,6 +16,7 @@ struct regionElement
 {
     int count;
     int score;
+    int score2;
 };
 
 void getSize(char *filename, int *cols, int *rows)
@@ -64,6 +66,7 @@ void loadData(struct element **arr, char *filename, int cols, int rows)
             {
                 arr[i][j].type = buf[j];
                 arr[i][j].contribution = -1;
+                arr[i][j].contribution2 = -1;
                 arr[i][j].region = -1;
             }
         }
@@ -114,6 +117,107 @@ void score(struct element **arr, int i, int j, int rows, int cols)
     arr[i][j].contribution = s;
 }
 
+void score2(struct element **arr, int i, int j, int rows, int cols)
+{
+    char type = arr[i][j].type;
+
+    int s = 0;
+
+    if (i == 0) // top
+    {
+        if (j == 0)
+        {
+            s++;
+        }
+        else if (arr[i][j-1].type != type)
+        {
+            s++;
+        }
+    }
+    else if (arr[i-1][j].type != type)
+    {
+        if (j == 0 || arr[i][j-1].type != type)
+        {
+            s++;
+        }
+        else if (arr[i][j-1].type == type && arr[i-1][j-1].type == type)
+        {
+            s++;
+        }
+    }
+
+    if (j == 0) // left
+    {
+        if (i == 0)
+        {
+            s++;
+        }
+        else if (arr[i-1][j].type != type)
+        {
+            s++;
+        }
+    }
+    else if (arr[i][j-1].type != type)
+    {
+        if (i == 0 || arr[i-1][j].type != type)
+        {
+            s++;
+        }
+        else if (arr[i-1][j].type == type && arr[i-1][j-1].type == type)
+        {
+            s++;
+        }
+    }
+
+    if (j == cols - 1) // right
+    {
+        if (i == 0)
+        {
+            s++;
+        }
+        else if (arr[i-1][j].type != type)
+        {
+            s++;
+        }
+    }
+    else if (arr[i][j+1].type != type)
+    {
+        if (i == 0 || arr[i-1][j].type != type)
+        {
+            s++;
+        }
+        else if (arr[i-1][j].type == type && arr[i-1][j+1].type == type)
+        {
+            s++;
+        }
+    }
+
+    if (i == rows - 1) // bottom
+    {
+        if (j == 0)
+        {
+            s++;
+        }
+        else if (arr[i][j-1].type != type)
+        {
+            s++;
+        }
+    }
+    else if (arr[i+1][j].type != type)
+    {
+        if (j == 0 || arr[i][j-1].type != type)
+        {
+            s++;
+        }
+        else if (arr[i][j-1].type == type && arr[i+1][j-1].type == type)
+        {
+            s++;
+        }
+    }
+
+    arr[i][j].contribution2 = s;
+}
+
 int main(int argc, char **argv)
 {
     // check
@@ -147,13 +251,14 @@ int main(int argc, char **argv)
         for (j = 0; j < cols; j++)
         {
             score(arr, i, j, rows, cols);
+            score2(arr, i, j, rows, cols);
 
             if (arr[i][j].region != -1) continue;
             explore(arr, i, j, rows, cols, &region);
             region++;
         }
     }
-
+    
     // alloc regions
     struct regionElement *regions = (struct regionElement *)malloc(region * sizeof(struct regionElement));
     for (i = 0; i < region; i++)
@@ -170,20 +275,27 @@ int main(int argc, char **argv)
         {
             int r = arr[i][j].region;
             int s = arr[i][j].contribution;
+            int s2 = arr[i][j].contribution2;
             
             regions[r].count++;
             regions[r].score += s;
+            regions[r].score2 += s2;
         }
     }
 
     // Part 1
     int part1 = 0;
+    int part2 = 0;
     for (i = 0; i < region; i++)
     {
         part1 += regions[i].count * regions[i].score;
+        part2 += regions[i].count * regions[i].score2;
+
+        //printf("Region %d has count %d, score2 %d, so res %d\n", i, regions[i].count, regions[i].score2, regions[i].count * regions[i].score2);
         //printf("Region %d has count %d, score %d, so res %d\n", i, regions[i].count, regions[i].score, regions[i].count * regions[i].score);
     }
     printf("Part 1: %d\n", part1);
+    printf("Part 2: %d\n", part2);
 
     // free
     free(regions);
